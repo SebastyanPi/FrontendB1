52e27430b3324a30460ea3373f6b7c8d12688e44
Sebastyan Pineda <sepineda@unicesar.edu.co>
2023-06-06 18:10:31 -0500

    Subiendo cambios

--- js/constante.js
+++ js/constante.js
@@ -1,5 +1,5 @@
 const CURL = "http://127.0.0.1:8000/api/";
-const PAGE = "http://localhost/B1final/FrontedWeb/";
+const PAGE = "http://localhost/FrontendB1/";
 var ventana = document.getElementById('onload');
 
 function loader(estado){
--- layout/constantes.php
+++ layout/constantes.php
@@ -1,4 +1,4 @@
 <?php
 $URL_API  = "http://127.0.0.1:8000/api/";
-$URL_WEB = "http://localhost/B1final/FrontedWeb/";
+$URL_WEB = "http://localhost/FrontendB1/";
 
